3fade107e0b5f30afd5c653a0057df386a93c978
Jadon Steinmetz <jadog123321@gmail.com>
2024-03-06 21:48:08 -0500

    Got chatgpt to create a basic frontend that I'll tune later

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from src.routers.prayerRequests import prayerRequestRouter
 from src.dependencies import repositories
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(prayerRequestRouter, prefix="/prayerRequests")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
 
 @app.on_event("shutdown")
 async def shutdown_event():
